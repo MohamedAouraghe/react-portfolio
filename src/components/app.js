2ef2536bde7967ff1f2faab0d80b07b37083c833
MohamedAouraghe <aouraghecode@gmail.com>
2023-04-14 10:28:22 +0200

    Second commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from './portfolio/portfolio-container';
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Mohamed Amin Portfolio</h1>
-        moment().format('MMMM Do YYYY, h:mm:ss a');
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />        
       </div>
     );
-  }
+  } 
 }
