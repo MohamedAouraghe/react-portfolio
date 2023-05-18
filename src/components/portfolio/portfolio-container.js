4ce471330786e6815b7ccc949980ad7f46131e2f
MohamedAouraghe <aouraghecode@gmail.com>
2023-05-18 16:26:28 +0200

    Adding content to Github

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -1,34 +1,94 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
-import PortfolioItem from './portfolio-item';
+import PortfolioItem from "./portfolio-item";
 
 export default class PortfolioContainer extends Component {
   constructor() {
     super();
 
     this.state = {
-      pageTitle: "Welcome to my Portfolio",
-      data: [
-        {title: "Quip"},
-        {title: "Eventbrite"}, 
-        {title: "Ministry Safe"}, 
-        {title: "SwingAway"}
-      ]
+      pageTitle: "Welcome to my portfolio",
+      isLoading: false,
+      data: []
+    };
+
+    this.handleFilter = this.handleFilter.bind(this);
+  }
+
+  handleFilter(filter) {
+    if (filter === "CLEAR_FILTERS") {
+      this.getPortfolioItems();
+    } else {
+      this.getPortfolioItems(filter);
     }
+  }
+
+  getPortfolioItems(filter = null) {
+    axios
+      .get("https://mohamedaminaouraghe.devcamp.space/portfolio/portfolio_items")
+      .then(response => {
+        if (filter) {
+          this.setState({
+            data: response.data.portfolio_items.filter(item => {
+              return item.category === filter;
+            })
+          });
+        } else {
+          this.setState({
+            data: response.data.portfolio_items
+          });
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      });
   }
 
   portfolioItems() {
     return this.state.data.map(item => {
-      return <PortfolioItem title={item.title} url={"google.com"} />;
-    })
+      return <PortfolioItem key={item.id} item={item} />;
+    });
   }
 
-  render () {
-    return (
-      <div>
-        <h2>{this.state.pageTitle}</h2>
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
 
-        {this.portfolioItems()}
+  render() {
+    if (this.state.isLoading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div className="homepage-wrapper">
+        <div className="filter-links">
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("eCommerce")}
+          >
+            eCommerce
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Scheduling")}
+          >
+            Scheduling
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("Enterprise")}
+          >
+            Enterprise
+          </button>
+          <button
+            className="btn"
+            onClick={() => this.handleFilter("CLEAR_FILTERS")}
+          >
+            All
+          </button>
+        </div>
+        <div className="portfolio-items-wrapper">{this.portfolioItems()}</div>
       </div>
     );
   }
